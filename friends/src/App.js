567b2dac6591d5342d291eed916a76b40e22b41d
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-24 16:25:22 -0600

    added a private route component and made the friends list a private route. It is currently working. Next I'll be moving to the form and post request

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Login from "./Components/Login"
 import FriendsList from './Components/FriendsList'
+import PrivateRoute from './Components/PrivateRoute'
 
 import './App.css';
 
@@ -10,7 +11,7 @@ function App() {
   return (
     <Router>
       <Route path='/login' component={Login}/>
-      <Route path='/friends' component={FriendsList} />
+      <PrivateRoute exact path='/friends' component={FriendsList} />
     </Router>
   );
 }
