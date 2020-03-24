3d0dd8e07161960b2d393a4a30453b8cb272e8a8
Michael Callahan <CallahanMichaelE@gmail.com>
2020-03-24 14:59:22 -0600

    inital axios call is working and now setting the token into local storage

--- friends/src/Components/Login.js
+++ friends/src/Components/Login.js
@@ -18,16 +18,26 @@ const Login = () => {
                 [e.target.name]: e.target.value
             }
         })
+        console.log(cred)
     }
 
     const submitLog = e => {
-        e.preventDefualt();
+        e.preventDefault();
         axiosWithAuth()
         //put in post request here
         //consolelog res to find where how payload for token needs to be set
         //set token in localstorage
         //then history push to friends page
         //add a catch just in case
+        .post('/api/login', cred.credentials)
+        .then(res => {
+            console.log("Login Response", res)
+            localStorage.setItem('token', JSON.stringify(res.data.payload))
+            // props.history.push('/friends')
+        })
+        .catch(err => {
+            console.log("There was an",err.response)
+        })
     }
 
     return (
